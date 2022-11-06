61136add28b380501bb862e291dae7fc8f0b4b7e
Mateqo <mateuszbartkowiak99@gmail.com>
2022-11-06 15:57:48 +0100

    University GetList (mock)

---
+++ back-end/UniversityApi/DbModels/University.cs
@@ -0,0 +1,16 @@
+﻿namespace UniversityApi.DbModels
+{
+    public class University
+    {
+        public int Id { get; set; }
+        public string? SubjectAreaName { get; set; }
+        public int SubjectAreaId { get; set; }
+        public string? UniversityName { get; set; }
+        public string? Country { get; set; }
+        public string? FlagUrl { get; set; }
+        public string? Website { get; set; }
+        public int AvailablePlaces { get; set; }
+        public decimal Rating { get; set; }
+        public bool IsObserved { get; set; }
+    }
+}
