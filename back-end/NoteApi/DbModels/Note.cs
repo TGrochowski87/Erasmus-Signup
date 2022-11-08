0f1bc395e57f058d03d6da3237c420cab961c279
konbra99 <krazi2255@gmail.com>
2022-11-08 18:37:27 +0100

    Rabbit dziala. Zaraz dodam lepszy przyklad. Dobrze by bylo przeniesc pakiety do Common.

---
+++ back-end/NoteApi/DbModels/Note.cs
@@ -0,0 +1,8 @@
+﻿namespace NoteApi.DbModels
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string? Content { get; set; }
+    }
+}
--- back-end/UniversityApi/Program.cs
+++ back-end/UniversityApi/Program.cs
@@ -1,4 +1,6 @@
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 using UniversityApi.DbModels;
 using UniversityApi.Repository;
 using UniversityApi.Service;
@@ -9,6 +11,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<UniversityDataContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("UniversityDb")));
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumers(Assembly.GetEntryAssembly());
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        cfg.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(false));
+        cfg.UseMessageRetry(retryConfigurator =>
+        {
+            retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
+        });
+    });
+});
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
