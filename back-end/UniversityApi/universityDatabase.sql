5178b448996f4c965e817f96a4ceabf49e9052a8
patplo1999 <patplo4@wp.pl>
2022-12-18 00:41:23 +0100

    insert changes

--- back-end/UniversityApi/universityDatabase.sql
+++ back-end/UniversityApi/universityDatabase.sql
@@ -101,8 +101,8 @@ CREATE TABLE pwr_subject (
     ects                    integer NOT NULL
 );
 
---INSERT_PERSIST_DOCKER_DATA
---subject_language
+--univeristyDB
+   --subject_language
 INSERT INTO subject_language (name) VALUES ('English');
 INSERT INTO subject_language (name) VALUES ('German');
 INSERT INTO subject_language (name) VALUES ('French');
@@ -213,15 +213,34 @@ INSERT INTO pwr_faculty (shortcut, name) VALUES ('W15','Off-campus Department WU
 --university
 INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('A GRAZ109','Fh Joanneum Gesellschaft MBH', 'Austria', 'Graz', 'info@fh-joanneum.at', 'https://www.fh-joanneum.at/');
 INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('B ANTWERP01','Universiteit Antwerpen', 'Belgium', 'Antwerp', 'voornaam.achternaam@student.uantwerpen.be', 'https://www.uantwerpen.be/nl/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('B MONS21','Université de Mons', 'Belgium', 'Mons', 'info.mons@umons.ac.be', 'https://web.umons.ac.be/fr/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('BG SOFIA06','Sofia University SV. Kliment Ohridski', 'Bulgaria', 'Sofia', 'intern@admin.uni-sofia.bg', 'https://www.uni-sofia.bg/eng');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('BG VARNA02','Tehnicheski Universitet-Varna', 'Bulgaria', 'Varna', 'erasmus@tu-varna.bg', 'https://www1.tu-varna.bg/tu-varna/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('CZ BRNO01','Brno University of Technology', 'Czechia', 'Brno', 'vut@vutbr.cz', 'https://www.vut.cz/en/');
 INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('CZ LIBEREC01','Technicka Univerzita v Liberci', 'Czechia', 'Liberec', 'info@tul.cz', 'https://www.tul.cz/en/erasmus-2/');
 INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('D BERLIN21','SRH Hochschule Berlin', 'Germany', 'Berlin', 'info.hsbe@srh.de', 'https://www.srh-berlin.de/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('D BREMEN04','Hochschule Bremen', 'Germany', 'Bremen', 'zll-support@hs-bremen.de', 'https://www.hs-bremen.de/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('D DARMSTA02','Hochschule Darmstadt', 'Germany', 'Darmstadt', 'helpdesk@h-da.de', 'https://h-da.de/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('E MURCIA01','Universidad de Murcia', 'Spain', 'Murcia', 'siinfo@um.es', 'https://www.um.es/en/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('E TENERIF01','Universidad de la Laguna', 'Spain', 'La Laguna', 'acceso@ull.es', 'https://www.ull.es/');
 INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('E SEVILLA01','Universidad de Sevilla', 'Spain', 'Sevilla', 'info@us.es', 'https://www.us.es/');
 INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('F MARSEILL84','Universite Aix-Marseille', 'France', 'Marseille', 'info@univ-amu.fr', 'https://www.univ-amu.fr/');
-INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('G KRITIS09','Polytechnio Kritis', 'Greece', 'Chania', ' dpo@tuc.gr', 'https://www.tuc.gr/index.php?id=5397');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('F NANTES 01','Universite de Nantes', 'France', 'Nantes', 'nom.prenom@univ-nantes.fr', 'https://www.univ-nantes.fr/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('F LILLE103','Universite de Lille', 'France', 'Lille', 'info@ul.fr', 'https://www.univ-lille.fr/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('F CHATENA02','Ecole Centrale Paris', 'France', 'Gif-sur-Yvette', 'communication@centralesupelec.fr', 'https://www.centralesupelec.fr/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('F RENNES01','Universite de Rennes', 'France', 'Rennes', 'info@univ-rennes1.fr', 'https://www.univ-rennes1.fr/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('G KRITIS09','Polytechnio Kritis', 'Greece', 'Chania', 'dpo@tuc.gr', 'https://www.tuc.gr/index.php?id=5397');
 INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('HR RIJEKA01','University of Rijeka', 'Croatia', 'Rijeka', 'ured@uniri.hr', 'https://uniri.hr/en/home/');
 INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('I PADOVA01','Universit Delgi Studi di Padova', 'Italy', 'Padova', 'amministrazione.centrale@pec.unipd.it', 'https://www.unipd.it/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('IRL LIMERIC01','University of Limerick', 'Ireland', 'Limerick', 'ulglobal@ul.ie', 'https://www.ul.ie/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('LV RIGA13','BA School of Business and Finance', 'Latvia', 'Riga', 'info@ba.lv', 'https://ba.lv/en/');
 INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('P PORTO02','Universidade do Porto', 'Portugal', 'Porto', 'info@up.pt', 'https://www.up.pt/portal/en/');
-
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('RO TIMISOA04','Universitatea Politehnica Timisoara', 'Romania', 'Lloyd Palace', 'rector@upt.ro', 'https://www.upt.ro/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('SK ZILINA01','Zlinska Univrzita v Ziline', 'Slovakia', 'Zlin', 'vzdelavanie@uniza.sk', 'https://www.uniza.sk/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('SI LJUBLJA01','University of Ljubijana', 'Slovenia', 'Ljubijana', 'student.office@uni-lj.si', 'https://www.uni-lj.si/university/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('SF ESPOO12','Aalto University', 'Finland', 'Helsinki', 'studentservices@aalto.fi', 'https://www.aalto.fi/en');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('S VAXJO03','Linnaeus University', 'Sweden', 'Växjö', 'admission@lnu.se', 'https://lnu.se/en/');
+INSERT INTO university (erasmus_code, name, country, city, email, link) VALUES ('TR ANKARA01','Ankara University', 'Turkey', 'Ankara', 'auprir@ankara.edu.tr', 'https://www.ankara.edu.tr/en/');
 --pwr_speciality
 INSERT INTO pwr_speciality (name,pwr_faculty_short) VALUES ('IS-II-IO','W4N');
 INSERT INTO pwr_speciality (name,pwr_faculty_short) VALUES ('IS-II-PSI','W4N');
@@ -258,6 +277,26 @@ INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,acc
 INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,FALSE,TRUE,'2','10','2015-11-18','2021-09-30');
 INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (FALSE,TRUE,FALSE,'2','4','2015-10-09','2021-09-30');
 INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,TRUE,FALSE,'1','10','2015-10-09','2021-09-30');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,FALSE,FALSE,'2','6','2022-09-02','2022-10-01');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,FALSE,FALSE,'2','5','2022-09-02','2021-09-30');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,TRUE,FALSE,'2','10','2022-03-21','2023-02-01');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,TRUE,FALSE,'2','5','2022-04-03','2022-09-30');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,TRUE,FALSE,'1','5','2022-04-03','2022-09-30');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,TRUE,FALSE,'1','10','2022-09-30','2023-09-01');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,TRUE,FALSE,'2','5','2022-09-02','2023-03-01');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,FALSE,TRUE,'2','4','2022-11-18','2023-03-12');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (FALSE,TRUE,FALSE,'1','5','2022-10-02','2023-09-01');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,TRUE,FALSE,'2','10','2023-10-02','2024-09-01');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,FALSE,FALSE,'2','6','2023-09-02','2023-10-01');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,FALSE,FALSE,'2','5','2023-09-02','2024-09-30');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,TRUE,FALSE,'2','10','2023-03-21','2024-02-01');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,TRUE,FALSE,'2','5','2023-04-03','2023-09-30');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,TRUE,FALSE,'1','5','2023-04-03','2023-09-30');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,TRUE,FALSE,'1','10','2023-09-30','2024-09-01');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,TRUE,FALSE,'2','5','2023-09-02','2024-03-01');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,FALSE,TRUE,'2','4','2023-11-18','2024-03-12');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (FALSE,TRUE,FALSE,'1','5','2023-10-02','2024-09-01');
+INSERT INTO contract_details (accepting_undergraduate,accepting_postgraduate,accepting_doctoral,vacancy.max_positions,vacancy.months,conclusion_date,expiration_date) VALUES (TRUE,TRUE,FALSE,'2','10','2023-10-02','2024-09-01');
 --dest_speciality
 INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('A GRAZ109','W4N','1','0610','2','24');
 INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('CZ LIBEREC01','W6','4','0724','10','7');
@@ -269,6 +308,26 @@ INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_det
 INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('B ANTWERP01','W2','5','0722','1','8');
 INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('P PORTO02','W3','7','0531','1','32');
 INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('I PADOVA01','W4N','6','0481','1','77');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('LV RIGA13','W3','11','0512','21','12');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('B MONS21','W3','12','0727','3','27');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('BG SOFIA06','W7','14','0443','1','23');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('BG VARNA02','W3','15','0914','1','45');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('CZ BRNO01','W13','13','0541','10','29');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('D BREMEN04','W4N','18','0611','2','36');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('D DARMSTA02','W5','20','0522','2','15');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('E MURCIA01','W11','19','0530','4','18');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('E TENERIF01','W4N','17','0480','4','22');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('F NANTES 01','W4N','16','0480','3','37');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('F LILLE103','W2','21','0700','3','14');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('F CHATENA02','W7','28','0850','3','47');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('F RENNES01','W9','30','0140','3','24');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('IRL LIMERIC01','W8N','22','0311','1','17');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('RO TIMISOA04','W11','29','0441','25','38');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('SK ZILINA01','W10','26','0521','28','16');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('SI LJUBLJA01','W1','23','0730','29','8');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('S VAXJO03','W3','27','0531','1','32');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('TR ANKARA01','W4N','24','0481','31','77');
+INSERT INTO dest_speciality (dest_university_code,pwr_faculty_short,contract_details_id,study_area_id,subject_language_id,interested_students) VALUES ('TR ANKARA01','W12N','25','0523','31','45');
 --min_grade_history
 INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('1','4.8','9');
 INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('3','4.95','9');
@@ -279,5 +338,25 @@ INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('7','4
 INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('5','4.8','8');
 INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('8','5.15','9');
 INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('9','5.02','9');
-INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('10','4.68','9');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('10','4.88','9');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('11','4.83','9');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('13','4.85','9');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('14','4.68','8');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('12','5.04','8');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('16','5.2','9');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('17','4.8','8');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('15','4.85','8');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('18','5.10','9');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('19','5.12','9');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('20','5.18','9');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('21','4.84','9');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('23','4.92','9');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('24','4.98','8');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('22','4.91','8');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('26','5.2','9');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('27','4.78','8');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('25','4.86','8');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('28','5.09','9');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('29','5.22','9');
+INSERT INTO min_grade_history (dest_speciality_id,grade,semester) VALUES ('30','4.88','9');
 
